2c7c341b335d102263b22d81f4afcb56836e1669
gemcfadyen <gemcfadyen@hotmail.co.uk>
2013-10-27 13:45:06 +0000

    configured the messages to be persistable and recoverable should the rabbitMQ server go down through the use of durables.

---
+++ src/main/java/workqueues/NewTask.java
@@ -0,0 +1,39 @@
+package workqueues;
+
+import java.io.IOException;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
+
+public class NewTask {
+
+	public static final String WORK_QUEUE = "WORK_QUEUE";
+
+	public static void main(String[] args) throws IOException {
+		ConnectionFactory connectionFactory = new ConnectionFactory();
+		connectionFactory.setHost("localhost");
+		Connection connection = connectionFactory.newConnection();
+
+		Channel channel = connection.createChannel();
+		channel.queueDeclare(WORK_QUEUE, isADurable(), false, false, null);
+		
+		String[] messagesThatTakeSomeTimeToProcess = new String[] { "hi.",
+				"Georgina..", "bye...", "another..", "lovely......", "message..." };
+
+		/*
+		 * Send a messages in one at a time - when there are multiple Workers running, a round-robin approach is taken.
+		 * RabbitMQ will route the next message to the next free Consumer.
+		 */
+		for (String individualMessage : messagesThatTakeSomeTimeToProcess) {
+			channel.basicPublish("", WORK_QUEUE, MessageProperties.PERSISTENT_TEXT_PLAIN, individualMessage.getBytes());
+			System.out.println("[x] sent '" + individualMessage + "'");
+		}
+	}
+
+	private static boolean isADurable() {
+		return true;
+	}
+
+}
---
+++ src/main/java/workqueues/Worker.java
@@ -0,0 +1,72 @@
+package workqueues;
+
+
+import static workqueues.NewTask.WORK_QUEUE;
+
+import java.io.IOException;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.QueueingConsumer;
+
+/**
+ * This mimics messages being sent to and from a receiver, where the messages
+ * are actually tasks that will take some time to complete. The
+ * 'mimicATimeConsumingTaskBeingExecuted' method spoofs this by sleeping for a
+ * few seconds thus faking work being completed.
+ * 
+ * @author Georgina
+ * 
+ */
+public class Worker {
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+		ConnectionFactory connectionFactory = new ConnectionFactory();
+		connectionFactory.setHost("localhost");
+		Connection connection = connectionFactory.newConnection();
+
+		Channel channel = connection.createChannel();
+		channel.queueDeclare(WORK_QUEUE, isADurable(), false, false, null);
+		System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+		QueueingConsumer consumer = new QueueingConsumer(channel);
+		
+		channel.basicConsume(WORK_QUEUE, isNotConfiguredToRedistributeMessagesIfWorkerGoesDown(), consumer); 
+		channel.basicQos(1);
+		
+		while (true) {
+			QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+			String message = new String(delivery.getBody());
+			System.out.println("[y] received the message '" + message + "'");
+			mimicATimeConsumingTaskBeingExecuted(message);
+			System.out.println("[y] has completed its work");
+			channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+		}
+
+	}
+
+	private static boolean isNotConfiguredToRedistributeMessagesIfWorkerGoesDown() {
+		return false;
+	}
+
+	private static boolean isADurable() {
+		return true;
+	}
+
+	/*
+	 * For each '.' in the input messages, the thread sleeps for one second,
+	 * mimicking different amounts of work being done.
+	 */
+	private static void mimicATimeConsumingTaskBeingExecuted(String task)
+			throws InterruptedException {
+
+		for (char charactersInMessage : task.toCharArray()) {
+			if (charactersInMessage == '.') {
+				System.out.println("Sleeping for a second");
+				Thread.sleep(1000);
+				System.out.println("Alive again");
+			}
+		}
+	}
+}
